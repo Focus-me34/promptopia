21ce3e88fbd6fcbcdaf840b4370d73f30c228e97
aminmallol <aminm-34@hotmail.fr>
2023-06-16 17:39:43 +0100

    Added the possibility to search for prompts using the search input.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -20,9 +20,23 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState(null);
 
   const handleSearchChange = (e) => {
     setSearchText(e.target.value);
+
+    const filteredPosts = posts.filter((post) => {
+      const searchValue = e.target.value.toLowerCase();
+
+    const hasMatchingTag = post?.tag?.toLowerCase().includes(searchValue);
+    const hasMatchingUsername = post?.creator?.username?.toLowerCase().includes(searchValue);
+    const hasMatchingEmail = post?.creator?.email?.toLowerCase().includes(searchValue);
+    const hasMatchingPrompt = post?.prompt?.toLowerCase().includes(searchValue);
+
+    return hasMatchingTag || hasMatchingUsername || hasMatchingEmail || hasMatchingPrompt;
+    });
+
+    setFilteredPosts(filteredPosts);
   };
 
   useEffect(() => {
@@ -47,7 +61,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}}/>
+      <PromptCardList data={filteredPosts ? filteredPosts : posts} handleTagClick={() => {}}/>
     </section>
   )
 }
